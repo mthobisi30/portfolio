249523484fede38019c61d0c6029bc04e7538874
Mthobsi Nxumalo <105317670+mthobisi30@users.noreply.github.com>
2026-02-07 13:42:46 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Portfolio from './App.jsx'
+import Portfolio from './app.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
